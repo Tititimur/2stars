3f8c918daa9bad352f14a18770e66b9bd7f796fa
Станислав Мурсалович Мурсалов <sm_mursalov@betboom.org>
2024-10-05 18:26:56 +0300

    Добавлен учёт даты

--- pass.js
+++ pass.js
@@ -1,6 +1,31 @@
+var userList = [
+    "Лапутин Тимофей",
+    "Воронкова Полина",
+    "Кузьминова Арина",
+    "Волкова Мария",
+    "Мурсалов Тимур",
+    "Сергеев Григорий"
+];
+
 function start() {
-    let now = new Date(Date.now());
-    jQuery('#date').text(now.toISOString());
+    fillUsers();
+}
+
+function fillUsers() {
+    let count = userList.length;
+
+    let start = "2024-09-30T00:00:00";
+    let dateStart = new Date(start);
+    let dayStart = Math.floor(dateStart.getTime() / 1000 / 3600 / 24);
+
+    let dayNow = Math.floor(Date.now() / 1000 / 3600 / 24);
+    let plus = Math.floor((dayNow+2-dayStart) / 7) % count;
+
+    for (var i = 0; i < count; i++) {
+        let j = (i+plus) % count;
+        let user = userList[j];
+        jQuery("#users").append('<li>' + user + '</li>');
+    }
 }
 
 function pass_submit() {
